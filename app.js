bb2a86a0a422b94372e8360331a2cd1ab8c42845
Dave Kuntz <dkuntz811@gmail.com>
2016-08-02 16:28:22 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+//each block needs to "know" its color
+$(.yellow).val(yellow);
+$(.red).val(red);
+$(.blue).val(blue);
+$(.green).val(green);
+	var blocks = [yellow, red, blue, green];
+
+
+
+
+
+
+
+
+
+
+	//Player needs to choose the right color. (on/click)
+
+
+	//If the player chooses correctly: message that.
+
+
+
+
+	//If the player chooses incorrectly: message that.
+
+
+
+	//After they have chosen the game needs to loop.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
