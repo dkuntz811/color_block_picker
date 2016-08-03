9a7dd5269de6e46c133150bfb42c355772851616
Dave Kuntz <dkuntz811@gmail.com>
2016-08-02 21:06:34 -0500

    Good enough?

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
 //each block needs to "know" its color
-$(.yellow).val(yellow);
-$(.red).val(red);
-$(.blue).val(blue);
-$(.green).val(green);
-	var blocks = [yellow, red, blue, green];
-
-
-
-
-
-
-
-
 
 
 	//Player needs to choose the right color. (on/click)
 
+
+	$('#yellow').on('click', function(event) {
+    $(this).append('<span>"YOU WERE RIGHT!"</span>');
+		console.log("hi");
+	});
+
+
+	$('#red').on('click', function(event) {
+		$(this).append('<span>"YOU WERE WRONG!"</span>');
+		$(this).append('<button>Play Again</button>')
+		console.log("it works");
+	});
+
+
+	$('#blue').on('click', function(event) {
+		$(this).append('<span>"YOU WERE WRONG!"</span>');
+		$(this).append('<button>Play Again</button>')
+		console.log("it works");
+	});
+
+	$('#green').on('click', function(event) {
+		$(this).append('<span>"YOU WERE WRONG!"</span>');
+		$(this).append('<button>Play Again</button>')
+		console.log("it works");
+	});
 
 	//If the player chooses correctly: message that.
 
