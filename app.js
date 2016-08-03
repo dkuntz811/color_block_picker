59436b793fcf577d876cbaa044977a3d2dbaef38
Dave Kuntz <dkuntz811@gmail.com>
2016-08-03 09:33:27 -0500

    progress?

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 //each block needs to "know" its color
 
-
+//var array = [yellow, red, blue green];
 	//Player needs to choose the right color. (on/click)
+//var yellow = 0;
+//var red = 0;
+//var blue = 0;
+//var green = 0;
+
+
+
 
 
 	$('#yellow').on('click', function(event) {
@@ -13,21 +20,27 @@ $(document).ready(function() {
 
 	$('#red').on('click', function(event) {
 		$(this).append('<span>"YOU WERE WRONG!"</span>');
-		$(this).append('<button>Play Again</button>')
+		$(this).append('<button>Play Again</button>');
+		$('<input type="button" value="Refresh Page" onClick="window.location.reload(true)">');
+
 		console.log("it works");
 	});
 
 
 	$('#blue').on('click', function(event) {
 		$(this).append('<span>"YOU WERE WRONG!"</span>');
-		$(this).append('<button>Play Again</button>')
+		$(this).append('<button>Play Again</button>');
+
 		console.log("it works");
 	});
 
 	$('#green').on('click', function(event) {
 		$(this).append('<span>"YOU WERE WRONG!"</span>');
-		$(this).append('<button>Play Again</button>')
+		$(this).append('<button>Play Again</button>');
+
 		console.log("it works");
+
+
 	});
 
 	//If the player chooses correctly: message that.
